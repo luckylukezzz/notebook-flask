2cb113390e862b0c870212c7ef430e1096ab2870
luckylukezzz <patalee2001@gmail.com>
2023-09-04 13:15:24 +0530

    login,sign up done

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,29 @@
-from flask import Blueprint,render_template,request , flash
+from flask import Blueprint,render_template,request ,redirect,url_for, flash
+from . import mysql
 
 auth = Blueprint('auth',__name__)   
 
 @auth.route('/login', methods=['POST','GET'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        cursor = mysql.connection.cursor()
+
+        # Check if the email is registered
+        cursor.execute("SELECT * FROM user WHERE email = %s", (email,))
+        existing_user = cursor.fetchone()
+
+        if not existing_user:
+            flash('Theres no registered users in this email address. try again', category='error')
+        else:
+            if existing_user[2] != password :
+                 flash('Incorrect password. try again',category='error')
+            else:
+                flash('Login success!',category='success')
+                return redirect(url_for('auth.logout'))
+
+
     return render_template('login.html')
 
 
@@ -21,16 +41,29 @@ def signUp():
 
         if len(email) < 4:
             flash("Email must contain more than 3 chars" , category='error')
-        if len(firstName) < 2:
+        elif len(firstName) < 2:
             flash("first name must contain more than 1 char" , category='error')
-        if len(password1) < 7:
-            flash("password must contain more than 7 chars" , category='error')
-        if password1 == password2:
-            flash("account creation successfull" , category='success')
-        else:
+        elif len(password1) < 3:
+            flash("password must contain more than 3 chars" , category='error')
+        elif password1 != password2:
             flash("passwords dosen't match" , category='error')
-            
-            
+        else: 
+            cursor = mysql.connection.cursor()
 
+             # Check if the email is already in use
+            cursor.execute("SELECT * FROM user WHERE email = %s", (email,))
+            existing_user = cursor.fetchone()
 
+            if existing_user:
+                flash('Email is already in use. Please choose a different one.', 'error')
+            else:
+                cursor.execute(
+                    "INSERT INTO user (email, first_name, password) VALUES (%s, %s, %s)",
+                    (email, firstName, password1)
+                )
+                mysql.connection.commit()
+                cursor.close()
+                flash("account creation successfull" , category='success')
+                return redirect(url_for('auth.login'))
+        
     return render_template('sign_up.html')
